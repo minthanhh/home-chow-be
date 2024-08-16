b449eeebaf1e8df30a7a4f563a746f1eefdc2982
minthanhh <mint03sanzz@gmail.com>
2024-08-16 23:08:59 +0700

    feat(config): Added more config

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,23 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
+import helmet from 'helmet'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     app.use(cookieParser())
+    app.use(helmet())
+    app.useGlobalPipes(new ValidationPipe())
 
-    const config = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').addTag('cats').build()
+    // eslint-disable-next-line prettier/prettier
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build()
 
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('api', app, document)
